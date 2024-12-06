9b6e106afb3ea61f941f4727f2e4e88d17e0bcbc
Adasf <osjegaar@gmail.com>
2024-12-05 21:43:07 -0700

    Refactor database connection configuration to use destructured environment variables for clarity and cleaner code

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,13 @@
 import mysql from "mysql2/promise";
 
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT } = process.env;
+
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  waitForConnections: true,
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
+  port: parseInt(DB_PORT),
   connectionLimit: 10,
   queueLimit: 0,
 });
